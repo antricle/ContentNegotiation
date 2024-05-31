b8bf12021a863851ba089f9084306c9fa100309d
manishchopra@myself.com <manishchopra@myself.com>
2024-05-31 05:33:16 +0100

    adding 406 and xml serializer formatter

--- ContentNegotiation/Program.cs
+++ ContentNegotiation/Program.cs
@@ -1,12 +1,11 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-builder.Services.AddControllers();
+builder.Services.AddControllers(options =>
+{
+    options.ReturnHttpNotAcceptable = true;
+}).AddXmlSerializerFormatters();
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
 
 app.UseAuthorization();
 
